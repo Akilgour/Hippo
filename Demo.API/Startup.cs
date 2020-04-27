f1708a88981fb67220fcad2535ebdf4adb0d4540
Ashley Kilgour <kilgourkilgour@hotmail.com>
2020-04-27 22:25:34 +0100

    Added error logging for the demo api

--- Demo.API/Startup.cs
+++ Demo.API/Startup.cs
@@ -1,9 +1,14 @@
 using Hippo;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
 
 namespace Demo.API
 {
@@ -27,10 +32,29 @@ namespace Demo.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler(eApp =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                eApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+
+                    var errorCtx = context.Features.Get<IExceptionHandlerFeature>();
+                    if (errorCtx != null)
+                    {
+                        var ex = errorCtx.Error;
+                        WebHelper.LogWebError("Demo", "API", ex, context);
+
+                        var errorId = Activity.Current?.Id ?? context.TraceIdentifier;
+                        var jsonResponse = JsonConvert.SerializeObject(new CustomErrorResponse
+                        {
+                            ErrorId = errorId,
+                            Message = "Some kind of error happened in the API."
+                        });
+                        await context.Response.WriteAsync(jsonResponse, Encoding.UTF8);
+                    }
+                });
+            });
 
             app.UseHttpsRedirection();
 
