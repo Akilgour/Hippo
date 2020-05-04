c965bcb77197372fd8264196b77450650ed013ba
Ashley Kilgour <kilgourkilgour@hotmail.com>
2020-05-04 20:59:30 +0100

    Added a domain project that will contrain all the database models

---
+++ Hippologamus.Domain/Models/DetailLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hippologamus.Domain.Models
+{
+    public class DetailLog
+    {
+        public string Id { get; set; }
+        public string Level { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Properties { get; set; }
+        public string LogEvent { get; set; }
+        public string MachineName { get; set; }
+        public string Assembly { get; set; }
+    }
+}
---
+++ Hippologamus.Domain/Models/UsageLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hippologamus.Domain.Models
+{
+    internal class UsageLog
+    {
+        public int Id { get; set; }
+        public string Level { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Properties { get; set; }
+        public string LogEvent { get; set; }
+        public string MachineName { get; set; }
+        public string Assembly { get; set; }
+    }
+}
