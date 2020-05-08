94b73d449f3078ece7025dbbff905f2deabcbda8
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-08 20:37:14 +0100

    Added API project

--- Hippologamus.Data/Context/HippologamusContext.cs
+++ Hippologamus.Data/Context/HippologamusContext.cs
@@ -5,14 +5,15 @@ namespace Hippologamus.Data.Context
 {
     public class HippologamusContext : DbContext
     {
+        public HippologamusContext(DbContextOptions<HippologamusContext> options)
+     : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
         public DbSet<DetailLog> DetailLogs { get; set; }
         public DbSet<DetailLogComment> DetailLogComments { get; set; }
         public DbSet<PerfLog> PerfLogs { get; set; }
         public DbSet<UsageLog> UsageLogs { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Logging");            
-        }
     }
 }
