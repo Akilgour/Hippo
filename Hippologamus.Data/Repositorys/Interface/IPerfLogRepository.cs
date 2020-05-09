061e363c6e49fd2ea793b2a00400c59be291e5b1
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-09 13:05:04 +0100

    Removed un using usings

--- Hippologamus.Data/Repositorys/Interface/IPerfLogRepository.cs
+++ Hippologamus.Data/Repositorys/Interface/IPerfLogRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Hippologamus.Domain.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Hippologamus.Data.Repositorys.Interface
