fe0a0d0d81f4280b4dc28280bc4673899708a323
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-09 13:26:00 +0100

    Added Autofac to data project

---
+++ Hippologamus.Data/Autofac/HippologamusDataModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Hippologamus.Data.Context;
+
+namespace Hippologamus.Data.Autofac
+{
+    public class HippologamusDataModule : Module
+    {
+        public HippologamusDataModule()
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .WithParameter("context", new HippologamusContext( ))
+                .AsImplementedInterfaces();
+        }
+    }
+}
