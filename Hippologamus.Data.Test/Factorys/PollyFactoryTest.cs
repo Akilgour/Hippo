6c9d23a48ff6c3d2026ecf6f269b0ec562d17b7a
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-11 15:19:24 +0100

    Added polly, this will be used when connecting to the db and ananther external connections. It uses a jitter approach, so it dosnt spam every two min, this means if there a a deadlock because two things tried to write at the same time, they do not retry at the same time. Added unit tests

---
+++ Hippologamus.Data.Test/Factorys/PollyFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Hippologamus.Data.Factorys;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hippologamus.Data.Test.Factorys
+{
+    public class PollyFactoryTest
+    {
+        [Fact]
+        public async Task CreateAsyncRetryPolicy()
+        {
+            //arrange
+            var retyPolicy = PollyFactory.CreateAsyncRetryPolicy();
+            int i = 0;
+
+            //act
+            Exception ex = await Assert.ThrowsAsync<Exception>(() => retyPolicy.ExecuteAsync(() =>
+            {
+                 i++;
+                 throw new Exception($"Run number {i}");
+            }));
+
+            //assert
+            Assert.Equal("Run number 6", ex.Message);
+        }
+    }
+}
---
+++ Hippologamus.Data/Factorys/PollyFactory.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.Contrib.WaitAndRetry;
+using Polly.Retry;
+using System;
+
+namespace Hippologamus.Data.Factorys
+{
+    public static class PollyFactory
+    {
+        public static AsyncRetryPolicy CreateAsyncRetryPolicy()
+        {
+
+            //Taken from https://github.com/Polly-Contrib/Polly.Contrib.WaitAndRetry#wait-and-retry-with-jittered-back-off
+
+            var delay = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(1), retryCount: 5);
+
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(delay);
+            return retryPolicy;
+        }
+    }
+}
