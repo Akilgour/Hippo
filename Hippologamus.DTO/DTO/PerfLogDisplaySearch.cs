ca1f6f1886453075f14e8f776c8f03455edefc50
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-16 11:43:57 +0100

    Added comments

--- Hippologamus.DTO/DTO/PerfLogDisplaySearch.cs
+++ Hippologamus.DTO/DTO/PerfLogDisplaySearch.cs
@@ -4,9 +4,22 @@ namespace Hippologamus.DTO.DTO
 {
     public class PerfLogDisplaySearch
     {
+        /// <summary>
+        /// The Assembly that will get returned, this is not a wild search it must be a exact match.
+        /// If left blank it will return all assemblys
+        /// </summary>
         public string Assembly { get; set; }
+
+        /// The PerfItem that will get returned, this is not a wild search it must be a exact match.
+        /// If left blank it will return all PerfItems
         public string PerfItem { get; set; }
+
+        /// Any perf items with a timestamp greater or equal to this will be returned
+        /// /If left blank it will return all
         public DateTime? DateFrom { get; set; }
+
+        /// Any perf items with a timestamp less than or equal to this will be returned
+        /// /If left blank it will return all
         public DateTime? DateTo { get; set; }
     }
 }
