5889c2a5f86b36a53c04046aa4e3a6e655f58ba8
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-19 15:33:53 +0100

    Added calls that just writes a bunch of stuff to the logs

---
+++ Demo.API/Controllers/RandomErrorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomErrorController : ControllerBase
+    {
+
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            Log.Information("Information Log");
+            Log.Error("Error Log");
+            Log.Verbose("Verbose Log");
+            Log.Fatal("Fatal Log");
+            Log.Warning("Warning Log");
+            Log.Debug("Debug Log");
+            throw new Exception();
+        }
+    }
+}
