cc573b135f635424e64506e7bee1c345c034f09e
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-25 14:11:45 +0100

    Made assembly be a row

---
+++ Hippologamus.Server/Components/PerfLogAssemblyRowBase.cs
@@ -0,0 +1,13 @@
+﻿using Hippologamus.DTO.DTO;
+using Microsoft.AspNetCore.Components;
+
+namespace Hippologamus.Server.Components
+{
+    public class PerfLogAssemblyRowBase : ComponentBase
+    {
+        private PerfLogAssemblyDisplay perfLogAssemblyDisplay;
+
+        [Parameter]
+        public PerfLogAssemblyDisplay PerssssfLogAssembly { get; set; }
+    }
+}
