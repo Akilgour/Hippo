f81ded51ba764fb7a9ae9438e0a0e036f664eb61
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-26 16:26:06 +0100

    Extracted the header infomations from the API result Not used it yet.

---
+++ Hippologamus.DTO/DTO/RootHeader.cs
@@ -0,0 +1,8 @@
+﻿
+public class RootPaginationHeader
+{
+    public int TotalCount { get; set; }
+    public int PageSize { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+}
--- Hippologamus.Server/Services/PerfLogDisplayService.cs
+++ Hippologamus.Server/Services/PerfLogDisplayService.cs
@@ -2,6 +2,7 @@
 using Hippologamus.Server.Services.Interface;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -21,8 +22,24 @@ namespace Hippologamus.Server.Services
                 var searchJson =
                 new StringContent(JsonSerializer.Serialize(search), Encoding.UTF8, "application/json");
 
-                return await JsonSerializer.DeserializeAsync<PerfLogDisplayRoot>
-                            (await _httpClient.GetStreamAsync($"api/PerfLogs?Assembly=Hippologamus.API&RequestPath=%2Fapi%2FPerfLogAssembly"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            //var foo = await JsonSerializer.DeserializeAsync<PerfLogDisplayRoot>
+            //            (await _httpClient.GetStreamAsync($"api/PerfLogs?Assembly=Hippologamus.API&RequestPath=%2Fapi%2FPerfLogAssembly" ), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            var result = await _httpClient.GetAsync($"api/PerfLogs?Assembly=Hippologamus.API&RequestPath=%2Fapi%2FPerfLogAssembly");
+
+
+            var pagination = result.Headers.GetValues("X-Pagination").First();
+            
+
+            var rootPaginationHeader =   JsonSerializer.Deserialize<RootPaginationHeader>
+                            (pagination, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+
+            var bar = await JsonSerializer.DeserializeAsync<PerfLogDisplayRoot>
+                            (await result.Content.ReadAsStreamAsync(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+
+            return bar;
         }
     }
 }
