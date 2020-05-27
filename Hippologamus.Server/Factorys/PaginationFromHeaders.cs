6368d0f2ebf2ce3cc3f41046a623525dbcf99abe
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-27 10:43:28 +0100

    Code cleaned up, moved the header extration to its own factory as it will be used again. Made the varable names be cleaner

---
+++ Hippologamus.Server/Factorys/PaginationFromHeaders.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Hippologamus.Server.Factorys
+{
+    public static class PaginationFromHeaders
+    {
+        public static RootPaginationHeader Get(HttpResponseMessage response)
+        {
+            var pagination = response.Headers.GetValues("X-Pagination").First();
+
+            return JsonSerializer.Deserialize<RootPaginationHeader>
+                            (pagination, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
--- Hippologamus.Server/Services/PerfLogDisplayService.cs
+++ Hippologamus.Server/Services/PerfLogDisplayService.cs
@@ -1,14 +1,9 @@
 ﻿using Hippologamus.DTO.DTO;
 using Hippologamus.Server.Factorys;
 using Hippologamus.Server.Services.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Hippologamus.Server.Services
 {
@@ -21,21 +16,11 @@ namespace Hippologamus.Server.Services
 
         public async Task<PerfLogDisplayRoot> PerfLogDisplaySearch(PerfLogDisplaySearch search)
         {
-            var result = await _httpClient.GetAsync($"api/PerfLogs?{ObjectToURLString.Create(search)}");
-
-
-            var pagination = result.Headers.GetValues("X-Pagination").First(); 
-            
-
-            var rootPaginationHeader =   JsonSerializer.Deserialize<RootPaginationHeader>
-                            (pagination, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-
-
-            var bar = await JsonSerializer.DeserializeAsync<PerfLogDisplayRoot>
-                            (await result.Content.ReadAsStreamAsync(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-
-            bar.PaginationHeader = rootPaginationHeader;
-            return bar;
+            var response = await _httpClient.GetAsync($"api/PerfLogs?{ObjectToURLString.Create(search)}");
+            var result = await JsonSerializer.DeserializeAsync<PerfLogDisplayRoot>
+                            (await response.Content.ReadAsStreamAsync(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            result.PaginationHeader = PaginationFromHeaders.Get(response);
+            return result;
         }
     }
 }
