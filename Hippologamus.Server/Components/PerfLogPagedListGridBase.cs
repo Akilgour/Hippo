862890eeb047114c0f907f1324507a6950384e2e
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-29 13:14:17 +0100

    Moved the gird out to its own component

---
+++ Hippologamus.Server/Components/PerfLogPagedListGridBase.cs
@@ -0,0 +1,11 @@
+﻿using Hippologamus.Server.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Hippologamus.Server.Components
+{
+    public class PerfLogPagedListGridBase : ComponentBase
+    {
+        [Parameter]
+        public PerfLogPagedList PerfLogPagedList { get; set; }
+    }
+}
