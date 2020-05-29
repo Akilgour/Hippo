543a6b03ce8cff3574805940b81a8952e4d9592f
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-05-29 13:01:17 +0100

    Moved table to its own component

---
+++ Hippologamus.Server/Components/PerfLogPagedListTableBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hippologamus.Server.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Hippologamus.Server.Components
+{
+    public class PerfLogPagedListTableBase : ComponentBase
+    {
+        [Parameter]
+        public PerfLogPagedList PerfLogPagedList { get; set; }
+    }
+}
