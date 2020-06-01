bda8da5e734c215fa36813c091a69cbe3e4a6ab2
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-01 10:23:39 +0100

    Add factory that makes the drop down values for sorting the results on perf overview

---
+++ Hippologamus.Server.Test/Factory/PerfLogsOrderByListFactoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Hippologamus.Server.Factorys;
+using System.Linq;
+using Xunit;
+
+namespace Hippologamus.Server.Test.Factory
+{
+    public class PerfLogsOrderByListFactoryTest
+    {
+        [Fact]
+        public void Create_Count()
+        {
+            //arrange
+            //act
+            var value = PerfLogsOrderByListFactory.Create();
+            //assert
+            Assert.Equal(3, value.Count);
+        }
+
+        [Theory]
+        [InlineData("ElapsedMilliseconds", "Elapsed Time")]
+        [InlineData("MachineName", "Machine Name")]
+        [InlineData("TimeStamp", "Time Stamp")]
+        public void Create_Contains(string key, string expectedValue)
+        {
+            //arrange
+            //act
+            var value = PerfLogsOrderByListFactory.Create();
+            //assert
+            var item = value.First(x => x.Key == key);
+            Assert.Equal(key, item.Key);
+            Assert.Equal(expectedValue, item.Value);
+        }
+    }
+}
---
+++ Hippologamus.Server/Factorys/PerfLogsOrderByListFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Hippologamus.Server.Factorys
+{
+    public static class PerfLogsOrderByListFactory
+    {
+        public static IDictionary<string, string> Create()
+        {
+            return new Dictionary<string, string>
+            {
+                {"ElapsedMilliseconds", "Elapsed Time" },
+                {"MachineName", "Machine Name" },
+                {"TimeStamp", "Time Stamp" }
+         
+            };
+        }
+    }
+}
