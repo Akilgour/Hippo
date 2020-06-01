8c675cb6a108eb6613e24b986d7fb863be1ab50f
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-01 13:09:05 +0100

    Added button for sorting on table headers, Added events for this

--- Hippologamus.Server/Components/PerfLogPagedListTableBase.cs
+++ Hippologamus.Server/Components/PerfLogPagedListTableBase.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Hippologamus.Server.Models;
+﻿using Hippologamus.Server.Models;
 using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace Hippologamus.Server.Components
 {
     public class PerfLogPagedListTableBase : ComponentBase
     {
         [Parameter]
+        public string OrderByValue { get; set; }
+
+        [Parameter]
+        public EventCallback OrderBy_ClickCallback { get; set; }
+
+        [Parameter]
+        public EventCallback<string> OrderByValueChanged { get; set; }
+
+
+        [Parameter]
         public PerfLogPagedList PerfLogPagedList { get; set; }
+
+        public async Task ToggleOrderTimeStamp()
+        {
+            await ToggleOrder("TimeStamp");
+        }
+
+        public async Task ToggleOrderElapsedTime()
+        {
+            await ToggleOrder("ElapsedTime");
+        }
+
+        public async Task ToggleOrderMachineName()
+        {
+            await ToggleOrder("MachineName");
+        }
+
+        public async Task ToggleOrder(string e)
+        {
+            OrderByValue = e;
+            await OrderByValueChanged.InvokeAsync(OrderByValue);
+            await OrderBy_ClickCallback.InvokeAsync(OrderByValue);
+        }
     }
 }
