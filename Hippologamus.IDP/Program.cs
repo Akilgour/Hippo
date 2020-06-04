06886e37c3d9401059cb550d381d0018c8290aab
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-04 20:57:27 +0100

    Added asp identity server and set up db

--- Hippologamus.IDP/Program.cs
+++ Hippologamus.IDP/Program.cs
@@ -2,12 +2,21 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using Hippologamus.IDP.Areas.Identity.Data;
+using Hippologamus.IDP.Context;
+using IdentityModel;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
 using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Hippologamus.IDP
 {
@@ -34,7 +43,89 @@ namespace Hippologamus.IDP
             try
             {
                 Log.Information("Starting host...");
-                CreateHostBuilder(args).Build().Run();
+                var host = CreateHostBuilder(args).Build();
+
+                // seed the database.  Best practice = in Main, using service scope
+                using (var scope = host.Services.CreateScope())
+                {
+                    try
+                    {
+                        var context = scope.ServiceProvider.GetService<UserDbContext>();
+
+                        // ensure the DB is migrated before seeding
+                        context.Database.Migrate();
+
+                        // use the user manager to create test users
+                        var userManager = scope.ServiceProvider
+                            .GetRequiredService<UserManager<ApplicationUser>>();
+
+                        var jack = userManager.FindByNameAsync("Jack").Result;
+                        if (jack == null)
+                        {
+                            jack = new ApplicationUser
+                            {
+                                UserName = "Jack",
+                                EmailConfirmed = true
+                            };
+
+                            var result = userManager.CreateAsync(jack, "P@ssword1").Result;
+                            if (!result.Succeeded)
+                            {
+                                throw new Exception(result.Errors.First().Description);
+                            }
+
+                            result = userManager.AddClaimsAsync(jack, new Claim[]{
+                                new Claim(JwtClaimTypes.Name, "Jack Torrance"),
+                                new Claim(JwtClaimTypes.GivenName, "Jack"),
+                                new Claim(JwtClaimTypes.FamilyName, "Torrance"),
+                                new Claim(JwtClaimTypes.Email, "jack.torrance@email.com"),
+                                new Claim("country", "BE")
+                            }).Result;
+
+                            if (!result.Succeeded)
+                            {
+                                throw new Exception(result.Errors.First().Description);
+                            }
+                        }
+
+                        var wendy = userManager.FindByNameAsync("Wendy").Result;
+                        if (wendy == null)
+                        {
+                            wendy = new ApplicationUser
+                            {
+                                UserName = "Wendy",
+                                EmailConfirmed = true
+                            };
+
+                            var result = userManager.CreateAsync(wendy, "P@ssword1").Result;
+                            if (!result.Succeeded)
+                            {
+                                throw new Exception(result.Errors.First().Description);
+                            }
+
+                            result = userManager.AddClaimsAsync(wendy, new Claim[]{
+                                new Claim(JwtClaimTypes.Name, "Wendy Torrance"),
+                                new Claim(JwtClaimTypes.GivenName, "Wendy"),
+                                new Claim(JwtClaimTypes.FamilyName, "Torrance"),
+                                new Claim(JwtClaimTypes.Email, "wendy.torrance@email.com"),
+                                new Claim("country", "NL")
+                            }).Result;
+
+                            if (!result.Succeeded)
+                            {
+                                throw new Exception(result.Errors.First().Description);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "An error occurred while seeding the database.");
+                    }
+                }
+
+                // run the web app
+                host.Run();
                 return 0;
             }
             catch (Exception ex)
