1a9120878146b1ae15ad0d53459495452678c166
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-05 17:16:29 +0100

    Added error grid list

---
+++ Hippologamus.Server/Components/ErrorLogPagedListGridBase.cs
@@ -0,0 +1,11 @@
+﻿using Hippologamus.Server.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Hippologamus.Server.Components
+{
+    public class ErrorLogPagedListGridBase : ComponentBase
+    {
+        [Parameter]
+        public ErrorLogPagedList ErrorLogPagedList { get; set; }
+    }
+}
