9b71c7398c30000e2a5ddee1f55b20b62b36a6f8
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-08 12:31:16 +0100

    Updated the DetailLogComment to have more properties

---
+++ Hippologamus.Data/Migrations/20200608112513_UpdateDetailLogComment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hippologamus.Data.Migrations
+{
+    public partial class UpdateDetailLogComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateOn",
+                table: "DetailLogComments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreateadBy",
+                table: "DetailLogComments",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OpenState",
+                table: "DetailLogComments",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "DetailLogComments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateOn",
+                table: "DetailLogComments");
+
+            migrationBuilder.DropColumn(
+                name: "CreateadBy",
+                table: "DetailLogComments");
+
+            migrationBuilder.DropColumn(
+                name: "OpenState",
+                table: "DetailLogComments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "DetailLogComments");
+        }
+    }
+}
--- Hippologamus.Domain/Models/DetailLogComment.cs
+++ Hippologamus.Domain/Models/DetailLogComment.cs
@@ -1,9 +1,15 @@
-﻿namespace Hippologamus.Domain.Models
+﻿using System;
+
+namespace Hippologamus.Domain.Models
 {
     public class DetailLogComment
     {
         public int Id { get; set; }
         public string Comment { get; set; }
         public int DetailLogId { get; set; }
+        public bool OpenState { get; set; }
+        public string CreateadBy { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
     }
 }
