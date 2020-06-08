5685cf58391567c21d9789a0bd5ae01b8a2b0351
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-08 16:46:48 +0100

    Stubed out a comment page

---
+++ Hippologamus.Server/Models/DetailLogCommentEdit.cs
@@ -0,0 +1,11 @@
+﻿namespace Hippologamus.Server.Models
+{
+    public class DetailLogCommentEdit
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public bool LinkedToDevOps { get; set; }
+        public string DevOpsId { get; set; }
+        public string CreateadBy { get; set; }
+    }
+}
---
+++ Hippologamus.Server/Pages/EditDetailLogCommentsBase.cs
@@ -0,0 +1,26 @@
+﻿using Hippologamus.Domain.Models;
+using Hippologamus.Server.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hippologamus.Server.Pages
+{
+    public class EditDetailLogCommentsBase : PageComponentBase
+    {
+        
+        [Parameter]
+        public int DetailLogId { get; set; }
+
+        public DetailLogCommentEdit DetailLogComment { get; set; }
+
+        protected override void OnInitialized()
+        {
+            DetailLogComment = new DetailLogCommentEdit();
+        }
+
+    }
+}
