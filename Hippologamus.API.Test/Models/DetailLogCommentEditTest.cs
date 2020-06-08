bd7a4667ffd9d82861f0af112b4d0a04dcd03dc5
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-08 17:10:53 +0100

    Added validation to the DetailLogCommentEdit model Added tests for this

---
+++ Hippologamus.API.Test/Models/DetailLogCommentEditTest.cs
@@ -0,0 +1,43 @@
+﻿using Hippologamus.Server.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Xunit;
+
+namespace Hippologamus.API.Test.Models
+{
+    public class DetailLogCommentEditTest
+    {
+        [Fact]
+        public void ErrorMessage_IsSet()
+        {
+            //arrange
+            var model = new DetailLogCommentEdit() { LinkedToDevOps = true };
+
+            //act
+            var contex = new ValidationContext(model);
+            var result = model.Validate(contex);
+
+
+            //assert
+            Assert.Single(result);
+            Assert.Equal("If linked to a dev ops itm, you must provide a devops Id.", result.First().ErrorMessage);
+            Assert.Single(result.First().MemberNames);
+            Assert.Equal("DevOpsId", result.First().MemberNames.First());
+
+        }
+
+        [Fact]
+        public void NoErrorMessage_IsSet()
+        {
+            //arrange
+            var model = new DetailLogCommentEdit() { LinkedToDevOps = true, DevOpsId = "425" };
+
+            //act
+            var contex = new ValidationContext(model);
+            var result = model.Validate(contex);
+
+            //assert
+            Assert.Empty(result);
+        }
+    }
+}
--- Hippologamus.Server/Models/DetailLogCommentEdit.cs
+++ Hippologamus.Server/Models/DetailLogCommentEdit.cs
@@ -1,11 +1,25 @@
-﻿namespace Hippologamus.Server.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hippologamus.Server.Models
 {
-    public class DetailLogCommentEdit
+    public class DetailLogCommentEdit : IValidatableObject
     {
         public int Id { get; set; }
         public string Comment { get; set; }
         public bool LinkedToDevOps { get; set; }
         public string DevOpsId { get; set; }
         public string CreateadBy { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            if (LinkedToDevOps = true && string.IsNullOrWhiteSpace(DevOpsId))
+            {
+                errors.Add(new ValidationResult("If linked to a dev ops itm, you must provide a devops Id.", new[] { nameof(DevOpsId) }));
+            }
+            return errors;
+        }
     }
 }
