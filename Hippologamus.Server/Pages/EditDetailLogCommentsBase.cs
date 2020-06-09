9d0485be02efbbae00c41bba751421a3c3e3cb0b
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-09 11:55:05 +0100

    Added all the text boxs Stubbed out event actions

--- Hippologamus.Server/Pages/EditDetailLogCommentsBase.cs
+++ Hippologamus.Server/Pages/EditDetailLogCommentsBase.cs
@@ -6,10 +6,18 @@ namespace Hippologamus.Server.Pages
 {
     public class EditDetailLogCommentsBase : PageComponentBase
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [Parameter]
         public int DetailLogId { get; set; }
 
         public DetailLogCommentEdit DetailLogComment { get; set; }
+
+        //used to store state of screen
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
 
         protected override void OnInitialized()
         {
@@ -18,12 +26,23 @@ namespace Hippologamus.Server.Pages
 
         protected async Task HandleValidSubmit()
         {
+            
+            StatusClass = "alert-success";
+            Message = "Comment successfully.";
+            Saved = true;
+
             await Task.Delay(100);
         }
 
         protected void HandleInvalidSubmit()
         {
+            StatusClass = "alert-danger";
+            Message = "There are some validation errors. Please try again.";
+        }
 
+        protected void NavigateBack()
+        {
+            NavigationManager.NavigateTo("/erroroverview");
         }
     }
 }
