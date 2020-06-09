13bd2cc886e9edee760009b85f7d658ce1f3269b
Ashley Kilgour <ashley.kilgour@technical-limits.com>
2020-06-09 17:33:43 +0100

    Styled the gird a little

--- Hippologamus.Server/Models/DetailLogCommentList.cs
+++ Hippologamus.Server/Models/DetailLogCommentList.cs
@@ -8,7 +8,6 @@ namespace Hippologamus.Server.Models
         public string Comment { get; set; }
         public bool LinkedToDevOps { get; set; }
         public string DevOpsId { get; set; }
-        public int DetailLogId { get; set; }
         public bool OpenState { get; set; }
         public string CreateadBy { get; set; }
         public DateTime CreateOn { get; set; }
